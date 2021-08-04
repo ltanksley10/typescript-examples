f759fd1a037b79d167ef6e87fa722c01ba4ba9d0
ltanksley10 <contactlthere@gmail.com>
2021-08-04 16:19:27 +0000

    function overloads

--- app.js
+++ app.js
@@ -31,3 +31,13 @@ function moveAnimal(animal) {
     console.log('Moving at Speed ' + speed);
 }
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+//type casting 
+//helps you tell typescript some value is of a specific type
+//accessing element in dom
+var userInputElement = document.getElementById('user-input');
+// or 
+// const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
+userInputElement.value = 'Hi there!';
+var errorBag = {
+    email: 'Not a valid email!'
+};
--- app.ts
+++ app.ts
@@ -22,6 +22,8 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+//function overloads help typescript infer return type
+function add(a: number, b: number): number;
 function add(a: Combinable, b: Combinable) {
     if (typeof a === 'string' || typeof b === 'string') {
         return a.toString() + b.toString();
